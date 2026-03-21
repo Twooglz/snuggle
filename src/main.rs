bf20f8aa7cb5c2b8cb57d7f7eaf519ce854b7705
Maya <maya.vdv@proton.ne>
2026-03-21 23:29:20 +0100

    cleaned up main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,16 @@ use direction::Direction;
 use input::InputHandler;
 
 use crossterm::{
-    event::{Event, KeyCode, KeyEvent, poll, read},
     execute,
-    style::{Color, SetBackgroundColor},
     terminal::{
         Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode,
         enable_raw_mode,
     },
 };
 use std::io;
-use std::io::Write;
-use std::time::{Duration, Instant};
+use std::time::Duration;
 
-use crate::{
-    board::Board,
-    render::{RenderBuffer, Tile},
-};
+use crate::board::Board;
 
 fn main() {
     let mut stdout = io::stdout();
@@ -50,44 +44,8 @@ fn main() {
         }
 
         board.update(&mut input_handler);
-
-        // // Register inputs
-        // loop {
-        //     let time_until_tick: Duration = tick_rate.saturating_sub(now.elapsed());
-
-        //     if time_until_tick <= Duration::ZERO {
-        //         break;
-        //     }
-
-        //     match read_key_events(time_until_tick) {
-        //         Some(key) => {
-        //             match key.code {
-        //                 KeyCode::Char('w') | KeyCode::Up => input_queue.add(Direction::Up),
-        //                 KeyCode::Char('s') | KeyCode::Down => input_queue.add(Direction::Down),
-        //                 KeyCode::Char('a') | KeyCode::Left => input_queue.add(Direction::Left),
-        //                 KeyCode::Char('d') | KeyCode::Right => input_queue.add(Direction::Right),
-        //                 KeyCode::Char('q') => break 'game_loop,
-        //                 _ => continue,
-        //             }
-
-        //             if key.code == KeyCode::Char('q') {
-        //                 break 'game_loop;
-        //             }
-        //         }
-        //         _ => continue,
-        //     }
-        // }
     }
 
     execute!(stdout, LeaveAlternateScreen).expect("Failed to leave alternate screen");
     disable_raw_mode().expect("Failed disabling raw mode...");
 }
-
-// fn read_key_events(duration: Duration) -> Option<KeyEvent> {
-//     if poll(duration).ok()? {
-//         if let Event::Key(key) = read().ok()? {
-//             return Some(key);
-//         }
-//     }
-//     None
-// }
